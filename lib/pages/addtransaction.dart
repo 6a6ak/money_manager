24c04f0811aba235cae6784e8c333461ffe1adbc
6a6ak <babak.ahari@gmail.com>
2024-07-30 22:21:10 +0200

    date selector

--- lib/pages/addtransaction.dart
+++ lib/pages/addtransaction.dart
@@ -9,6 +9,7 @@ class AddTransaction extends StatefulWidget {
 
 class _AddTransactionState extends State<AddTransaction> {
   String type = 'Income'; //for btn
+  DateTime selectedDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -22,113 +23,112 @@ class _AddTransactionState extends State<AddTransaction> {
         ),
       ),
       body: ListView(
-          padding: const EdgeInsets.all(15),
-          children: [
-      Container(
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Colors.lightBlue.shade50, //bg color
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: const TextField(
-        keyboardType: TextInputType.number,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Amount',
-          hintText: 'Amount',
-          prefixIcon: Icon(
-            Icons.money,
-            color: Colors.green,
+        padding: const EdgeInsets.all(15),
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.lightBlue.shade50, //bg color
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Amount',
+                hintText: 'Amount',
+                prefixIcon: Icon(
+                  Icons.money,
+                  color: Colors.green,
+                ),
+              ),
+            ),
           ),
-        ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.lightBlue.shade50,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const TextField(
+              keyboardType: TextInputType.number,
+              maxLength: 30,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Description',
+                hintText: 'Write a description ',
+                prefixIcon: Icon(
+                  Icons.money,
+                  color: Colors.green,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ChoiceChip(
+                label: Text(
+                  'Cost',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: type == 'Cost' ? Colors.white : Colors.indigo),
+                ),
+                selected: type == 'Cost' ? true : false,
+                onSelected: (val) {
+                  if (val) {
+                    setState(() {
+                      type = 'Cost';
+                    });
+                  }
+                },
+                selectedColor: Colors.indigo,
+              ),
+             const SizedBox(height: 100),
+              ChoiceChip(
+                label: Text(
+                  'Income',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: type == 'Income' ? Colors.white : Colors.indigo),
+                ),
+                selected: type == 'Income' ? true : false,
+                onSelected: (val) {
+                  if (val) {
+                    setState(() {
+                      type = 'Income';
+                    });
+                  }
+                },
+                selectedColor: Colors.indigo,
+              ),
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  '${selectedDate.month}-${selectedDate.day}',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: null,
+              child: Text(
+                'Add',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+          ),
+        ],
       ),
-    ),
-    const SizedBox(
-    height: 10,
-    ),
-    Container(
-    padding: const EdgeInsets.all(8),
-    decoration: BoxDecoration(
-    color: Colors.lightBlue.shade50,
-    borderRadius: BorderRadius.circular(10),
-    ),
-    child: const TextField(
-    keyboardType: TextInputType.number,
-    maxLength: 30,
-    decoration: InputDecoration(
-    border: OutlineInputBorder(),
-    labelText: 'Description',
-    hintText: 'Write a description ',
-    prefixIcon: Icon(
-    Icons.money,
-    color: Colors.green,
-    ),
-    ),
-    ),
-    ),
-    const SizedBox(height: 20),
-    Row(
-    mainAxisAlignment: MainAxisAlignment.spaceAround,
-    children: [
-    ChoiceChip(
-    label: Text('Cost',
-    style: TextStyle(
-    fontSize: 20,
-    color: type == 'Cost' ? Colors.white : Colors.indigo),
-    ),
-      selected: type == 'Cost' ? true : false,
-      onSelected: (val) {
-        if (val) {
-          setState(() {
-            type = 'Cost';
-          });
-        }
-      },
-    selectedColor: Colors.indigo,
-    ),
-    SizedBox(height: 100),
-    ChoiceChip(
-    label: Text(
-    'Income',
-    style: TextStyle(
-    fontSize: 20,
-    color: type == 'Income' ? Colors.white : Colors.indigo),
-    ),
-    selected: type == 'Income' ? true : false,
-    onSelected: (val) {
-    if (val) {
-    setState(() {
-    type = 'Income';
-    });
-    }
-    },
-    selectedColor: Colors.indigo,
-    ),
-    SizedBox(height: 20),
-    TextButton(
-    onPressed: null,
-    child: Text(
-    'Trans Date',
-    style: TextStyle(fontSize: 20, color: Colors.white),
-    ),
-    ),
-    ],
-    ),
-    const SizedBox(height: 20),
-    const Padding(
-    padding: EdgeInsets.all(8.0),
-    child: ElevatedButton(
-    onPressed: null,
-    child: Text(
-    'Add',
-    style: TextStyle(color: Colors.white, fontSize: 20),
-    ),
-    ),
-    ),
-    ]
-    ,
-    )
-    ,
     );
   }
 }
